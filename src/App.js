8cebf6c7fbf943877b042287c095340ef10e6be0
Joseph <josephtsegen10@gmail.com>
2021-04-08 15:46:26 +0100

    i change the api due to blockage lol

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect, Fragment } from "react";
 import "./App.css";
 import InitialCurrency from "./component/InitialCurrency";
 import FinalCurrency from "./component/FinalCurrency";
+import "./staticData.json";
 
 function App() {
   // hooks
-  const [currency, setCurrency] = useState([]);
-  const [currencyList, setCurrencyList] = useState([]);
+  const staticData = require("./staticData.json");
+  const [currency, setCurrency] = useState(staticData);
+  const [currencyList, setCurrencyList] = useState(
+    [...Object.keys(staticData.rates)].sort()
+  );
   // data from the api
   useEffect(() => {
     async function fetchData() {
-      const URL = "https://api.exchangeratesapi.io/latest";
+      const URL =
+        "https://api.currencyfreaks.com/latest?apikey=87588fa449c74031b56efad8e4e93f73";
       const response = await fetch(URL);
       const data = await response.json();
       setCurrency(data);
+      console.log(data);
       setCurrencyList([data?.base, ...Object.keys(data?.rates)].sort()); // set current list as array starting with the base currency and adding the rest
     }
     fetchData();
@@ -33,12 +39,12 @@ function App() {
       return;
     } else if (currencyFrom.value === currencyTo.value) {
       output.value = input.value;
-    } else if (currencyTo.value === "EUR") {
+    } else if (currencyTo.value === "USD") {
       output.value =
         parseFloat(input.value) / parseFloat(Rates[initialCurrency]);
-    } else if (currencyFrom.value === "EUR") {
+    } else if (currencyFrom.value === "USD") {
       output.value = parseFloat(input.value) * parseFloat(Rates[finalCurrency]);
-    } else if (currencyFrom !== "EUR" && currencyTo !== "EUR") {
+    } else if (currencyFrom !== "USD" && currencyTo !== "USD") {
       output.value =
         (input.value / Rates[initialCurrency]) * Rates[finalCurrency];
     }
@@ -54,7 +60,8 @@ function App() {
             onClick={(e) => {
               e.preventDefault();
               convertForward();
-            }}>
+            }}
+          >
             {" "}
             ={" "}
           </button>
