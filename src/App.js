a97dc17aefa3aebe74f749cdc3b84ff0146ac2e2
Joseph <josephtsegen10@gmail.com>
2021-04-10 21:52:02 +0100

    api key => nothing to see here

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import React, { useState, useEffect, Fragment } from "react";
 import "./App.css";
 import InitialCurrency from "./component/InitialCurrency";
 import FinalCurrency from "./component/FinalCurrency";
+import "./StaticData.json";
+
+// static data for incase of api blockage or other factors
+function useStaticData() {
+  const StaticData = require("./StaticData.json");
+  return StaticData;
+}
 
 function App() {
+  let StaticData = useStaticData();
   // hooks
-  const [currency, setCurrency] = useState([]);
-  const [currencyList, setCurrencyList] = useState([]);
+  const [currency, setCurrency] = useState(StaticData);
+  const [currencyList, setCurrencyList] = useState(
+    [...Object.keys(StaticData?.rates)].sort()
+  );
   // data from the api
-  /*  useEffect(() => {
+  useEffect(() => {
     async function fetchData() {
-      const URL = "https://api.exchangeratesapi.io/latest";
+      const URL = `https://api.currencyfreaks.com/latest?apikey=${process.env.REACT_APP_CURRENCYFREAKS_API_KEY}`;
       const response = await fetch(URL);
       const data = await response.json();
+      console.log(data);
       setCurrency(data);
-      setCurrencyList([data?.base, ...Object.keys(data?.rates)].sort()); // set current list as array starting with the base currency and adding the rest
+      setCurrencyList([...Object.keys(data?.rates)].sort()); // setting the currency list as an array of currency shortnames in alphabetical order
     }
     fetchData();
-  }, []) */
+  }, []);
   // selectors
   const input = document.querySelector("#input");
   const output = document.querySelector("#output");
@@ -25,20 +36,20 @@ function App() {
   const currencyTo = document.querySelector(".currency-to");
   // Function to convert the currencies
   const convertForward = () => {
-    let initialCurrency = currencyFrom?.value;
-    let finalCurrency = currencyTo?.value;
-    let Rates = currency?.rates;
+    let initialCurrency = currencyFrom.value;
+    let finalCurrency = currencyTo.value;
+    let Rates = currency.rates;
     if (input.value === "") {
       output.value = "";
       return;
     } else if (currencyFrom.value === currencyTo.value) {
       output.value = input.value;
-    } else if (currencyTo.value === "EUR") {
+    } else if (currencyTo.value === "USD") {
       output.value =
         parseFloat(input.value) / parseFloat(Rates[initialCurrency]);
-    } else if (currencyFrom.value === "EUR") {
+    } else if (currencyFrom.value === "USD") {
       output.value = parseFloat(input.value) * parseFloat(Rates[finalCurrency]);
-    } else if (currencyFrom !== "EUR" && currencyTo !== "EUR") {
+    } else if (currencyFrom !== "USD" && currencyTo !== "USD") {
       output.value =
         (input.value / Rates[initialCurrency]) * Rates[finalCurrency];
     }
@@ -51,7 +62,9 @@ function App() {
         <form>
           <InitialCurrency currencyOption={currencyList} />
           <button
-            onClick={() => {
+            className="convert-button"
+            onClick={(e) => {
+              e.preventDefault();
               convertForward();
             }}
           >
