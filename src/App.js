baf33bbdce94340fc88faebcf1faad9f67a1cdce
Joseph <josephtsegen10@gmail.com>
2021-04-10 04:30:44 +0100

    reverted to previous commit before i tried to add material ui and failed

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React, { useState, useEffect, Fragment } from "react";
 import "./App.css";
 import InitialCurrency from "./component/InitialCurrency";
 import FinalCurrency from "./component/FinalCurrency";
-import "./staticData.json";
 
 function App() {
   // hooks
-  const staticData = require("./staticData.json");
-  const [currency, setCurrency] = useState(staticData);
-  const [currencyList, setCurrencyList] = useState(
-    [...Object.keys(staticData.rates)].sort()
-  );
+  const [currency, setCurrency] = useState([]);
+  const [currencyList, setCurrencyList] = useState([]);
   // data from the api
   /*  useEffect(() => {
     async function fetchData() {
-      const URL =
-        "https://api.currencyfreaks.com/latest?apikey=87588fa449c74031b56efad8e4e93f73";
+      const URL = "https://api.exchangeratesapi.io/latest";
       const response = await fetch(URL);
       const data = await response.json();
       setCurrency(data);
-      console.log(data);
-      setCurrencyList([...Object.keys(data?.rates)].sort()); // set current list as array starting with the base currency and adding the rest
+      setCurrencyList([data?.base, ...Object.keys(data?.rates)].sort()); // set current list as array starting with the base currency and adding the rest
     }
     fetchData();
   }, []) */
@@ -37,15 +31,17 @@ function App() {
     if (input.value === "") {
       output.value = "";
       return;
-    }
-    if (currencyFrom.value === currencyTo.value) output.value = input.value;
-    if (currencyTo.value === "USD")
+    } else if (currencyFrom.value === currencyTo.value) {
+      output.value = input.value;
+    } else if (currencyTo.value === "EUR") {
       output.value =
         parseFloat(input.value) / parseFloat(Rates[initialCurrency]);
-    if (currencyFrom.value === "USD")
+    } else if (currencyFrom.value === "EUR") {
       output.value = parseFloat(input.value) * parseFloat(Rates[finalCurrency]);
-    return (output.value =
-      (input.value / Rates[initialCurrency]) * Rates[finalCurrency]);
+    } else if (currencyFrom !== "EUR" && currencyTo !== "EUR") {
+      output.value =
+        (input.value / Rates[initialCurrency]) * Rates[finalCurrency];
+    }
   };
 
   return (
